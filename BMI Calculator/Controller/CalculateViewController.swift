ba1ff7d543a8bb26f693a048ea87fdffedf96389
Furkan Ceylan <furkn.ceyln@gmail.com>
2024-04-14 15:13:18 +0300

    CalculateViewController has been  created

---
+++ BMI Calculator/Controller/CalculateViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CalculateViewController.swift
+//  BMI Calculator
+//
+//  Created by Furkan Ceylan on 14.04.2024.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
